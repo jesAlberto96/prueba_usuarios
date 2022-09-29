73d6b636ae63a3b4d35c294d13831e8a63ef6a6a
Alberto Garcia <programador@lcnidiomas.com>
2022-09-29 18:15:34 -0500

    first commit

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\User;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+// use Swagger\Annotations as SWG;
+
+class ApiController extends AbstractFOSRestController
+{
+    public function getRegisterAction(Request $request, UserPasswordEncoderInterface $encoder) {
+        $serializer = $this->get('jms_serializer');
+        return new Response($serializer->serialize("holi", "json"));
+
+        $em = $this->getDoctrine()->getManager();
+
+        $user = [];
+        $message = "";
+
+        try {
+            $code = 200;
+            $error = false;
+
+            $name = $request->request->get('_name');
+            $email = $request->request->get('_email');
+            $username = $request->request->get('_username');
+            $password = $request->request->get('_password');
+
+            $user = new User();
+            $user->setName($name);
+            $user->setEmail($email);
+            $user->setUsername($username);
+            $user->setPlainPassword($password);
+            $user->setPassword($encoder->encodePassword($user, $password));
+
+            $em->persist($user);
+            $em->flush();
+
+        } catch (Exception $ex) {
+            $code = 500;
+            $error = true;
+            $message = "An error has occurred trying to register the user - Error: {$ex->getMessage()}";
+        }
+
+        $response = [
+            'code' => $code,
+            'error' => $error,
+            'data' => $code == 200 ? $user : $message,
+        ];
+
+        return new Response($serializer->serialize($response, "json"));
+    }
+}
